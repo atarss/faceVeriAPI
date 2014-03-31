207cbf9fee3124ca4694176100b8aee0b9063def
atarss <andy9999678@gmail.com>
2014-03-31 17:35:26 +0800

    add files

---
+++ faceVeri/html/script/script.js
@@ -0,0 +1,273 @@
+////script.js
+
+var leftReader = new FileReader();
+var rightReader = new FileReader();
+var leftImgObj = {}, rightImgObj = {};
+var resultArr = new Array();
+
+var hasClickedDetection = false;
+var hasDetected = false;
+var isComparing = false;
+var leftSelected = -1;
+var rightSelected = -1;
+var maxDisplayHeight;
+var detectionTime;
+
+var consoleMessageStr = "<h3 class='console_message'>Please select a face by clicking on a face.</h3>";
+var consoleErrorStr = "<p class='console_error'>ERROR : No faces were found on this picture.</p>";
+
+leftReader.onloadend = function(){
+  $("#left_img").attr("src", this.result);
+  var tmpImg = new Image();
+  tmpImg.onload = function(){
+  	leftImgObj.originalHeight = this.height;
+  	leftImgObj.originalWidth = this.width;
+  };
+  tmpImg.src = this.result;
+  leftImgObj.displayHeight = $("#left_img").height();
+  leftImgObj.displayWidth = $("#left_img").width();
+}; 
+
+rightReader.onloadend = function(){
+  $("#right_img").attr("src", this.result);
+  var tmpImg = new Image();
+  tmpImg.onload = function(){
+  	rightImgObj.originalHeight = this.height;
+  	rightImgObj.originalWidth = this.width;
+  };
+  tmpImg.src = this.result;
+  rightImgObj.displayHeight = $("#right_img").height();
+  rightImgObj.displayWidth = $("#right_img").width();
+};
+
+function comparePicMainFunc() {
+  // $(this).attr("disabled","disabled")
+  // $("#submit_button").attr("value", "Loading......");
+  $("#submit_button").css("display", "none");
+
+  leftImgObj.displayHeight = $("#left_img").height();
+  leftImgObj.displayWidth = $("#left_img").width();
+  rightImgObj.displayHeight = $("#right_img").height();
+  rightImgObj.displayWidth = $("#right_img").width();
+
+  $(".buttonbox").css("display", "none");
+  $(".submit_box").find("p").prepend("<img src='image/loading.gif' id='loading_gif'>");
+  
+  $(".file_button").css("display", "none");
+  
+  if ($("#left_img").height() > $("#right_img").height()) {
+    maxDisplayHeight = $("#left_img").height();
+  } else {
+    maxDisplayHeight = $("#right_img").height();
+  }
+  $(".picbox").css('height',maxDisplayHeight+"px");
+  $(".mainpicbox").css('height',maxDisplayHeight+"px");
+
+  // if (leftImgObj.displayHeight < maxDisplayHeight) {
+  //   $("#pic_paper_1").css('top', parseInt((maxDisplayHeight - leftImgObj.displayHeight)/2)+"px");
+  //   $("#pic_box_1").css('top', parseInt((maxDisplayHeight - leftImgObj.displayHeight)/2)+"px");
+  //  }
+
+  // if (rightImgObj.displayHeight < maxDisplayHeight) {
+  //   $("#pic_paper_2").css('top', parseInt((maxDisplayHeight - rightImgObj.displayHeight)/2)+"px");
+  //   $("#pic_box_2").css('top', parseInt((maxDisplayHeight - rightImgObj.displayHeight)/2)+"px");
+  // }
+
+  console.log("loading...");
+  $("#upload_form").ajaxSubmit({
+    dataType : "json",
+    success : function(resp,status,xhr,jq){
+      console.log(resp);
+      detectionTime = resp.time;
+      hasDetected = true;
+      
+      leftImgObj.serverId = resp.img1.id;
+      leftImgObj.result = resp.img1.result;
+      rightImgObj.serverId = resp.img2.id;
+      rightImgObj.result = resp.img2.result;
+      $("#paper_container_1").prepend("<div id='pic_paper_1' class='pic_paper'></div>");
+      $("#pic_paper_1").height(leftImgObj.displayHeight - 1).width(leftImgObj.displayWidth - 1);
+      var leftImgOffset = parseInt((580 - leftImgObj.displayWidth) / 2);
+      $("#pic_paper_1").css("left",leftImgOffset);
+      var leftScale = $("#left_img").width() / leftImgObj.originalWidth;
+      for (i=0;i<leftImgObj.result.length;i++) {
+        $("#pic_paper_1").prepend("<div class='pic_paper_container' id='left_box_container_"+i+"'></div>");
+        $("#left_box_container_"+i).prepend("<div class='pic_paper_box' id='left_box_"+i+"'></div>");
+
+        $("#left_box_"+i).width(parseInt(leftImgObj.result[i].w*leftScale)).height(parseInt(leftImgObj.result[i].h*leftScale));
+        $("#left_box_"+i).css('left',parseInt(leftImgObj.result[i].x*leftScale)).css('top',parseInt(leftImgObj.result[i].y*leftScale));
+        $("#left_box_"+i).mouseenter(function(){
+          var thisId = parseInt($(this).attr("id").slice(9));
+          if (leftSelected != thisId) {
+            $(this).css('background-color','rgba(255,255,255,0.5)');
+            $(this).find("canvas").css("display", "inline-block");
+          }
+        }).mouseleave(function(){
+          var thisId = parseInt($(this).attr("id").slice(9));
+          if (leftSelected != thisId) {
+            $(this).css('background-color','transparent');
+            $(this).find("canvas").css("display", "none");
+          }
+        });
+
+        $("#left_box_"+i).prepend("<canvas id='left_canvas_"+i+"' width="+parseInt(leftImgObj.result[i].w*leftScale)+" height="+parseInt(leftImgObj.result[i].h*leftScale)+" />");
+        var tmpCanvas = document.getElementById('left_canvas_'+i).getContext("2d");
+        for (j=0;j<27;j++){
+          var transX = (leftImgObj.result[i].points[j].x - leftImgObj.result[i].x)*leftScale;
+          var transY = (leftImgObj.result[i].points[j].y - leftImgObj.result[i].y)*leftScale;
+          tmpCanvas.beginPath();
+          tmpCanvas.arc(transX, transY, 1, 0, 2*Math.PI);
+          tmpCanvas.fillStyle = 'red';
+          tmpCanvas.fill();
+        }
+      }
+
+
+      $("#paper_container_2").prepend("<div id='pic_paper_2' class='pic_paper'></div>");
+      $("#pic_paper_2").height(rightImgObj.displayHeight - 1).width(rightImgObj.displayWidth - 1);
+      var rightImgOffset = parseInt((580 - rightImgObj.displayWidth) / 2);
+      $("#pic_paper_2").css("left",rightImgOffset);
+      var rightScale = $("#right_img").width() / rightImgObj.originalWidth;
+      for (i=0;i<rightImgObj.result.length;i++) {
+        $("#pic_paper_2").prepend("<div class='pic_paper_container' id='right_box_container_"+i+"'></div>");
+        $("#right_box_container_"+i).prepend("<div class='pic_paper_box' id='right_box_"+i+"'></div>");
+
+        $("#right_box_"+i).width(parseInt(rightImgObj.result[i].w*rightScale)).height(parseInt(rightImgObj.result[i].h*rightScale));
+        $("#right_box_"+i).css('left',parseInt(rightImgObj.result[i].x*rightScale)).css('top',parseInt(rightImgObj.result[i].y*rightScale));
+        $("#right_box_"+i).mouseenter(function(){
+          var thisId = parseInt($(this).attr("id").slice(10));
+          if (rightSelected != thisId) {
+            $(this).css('background-color','rgba(255,255,255,0.5)');
+            $(this).find("canvas").css("display", "inline-block");
+          }
+        }).mouseleave(function(){
+          var thisId = parseInt($(this).attr("id").slice(10));
+          if (rightSelected != thisId) {
+            $(this).css('background-color','transparent');
+            $(this).find("canvas").css("display", "none");
+          }
+        });
+
+        $("#right_box_"+i).prepend("<canvas id='right_canvas_"+i+"' width="+parseInt(rightImgObj.result[i].w*rightScale)+" height="+parseInt(rightImgObj.result[i].h*rightScale)+" />");
+        var tmpCanvas = document.getElementById('right_canvas_'+i).getContext("2d");
+        for (j=0;j<27;j++){
+          var transX = (rightImgObj.result[i].points[j].x - rightImgObj.result[i].x)*rightScale;
+          var transY = (rightImgObj.result[i].points[j].y - rightImgObj.result[i].y)*rightScale;
+          tmpCanvas.beginPath();
+          tmpCanvas.arc(transX, transY, 1, 0, 2*Math.PI);
+          tmpCanvas.fillStyle = 'red';
+          tmpCanvas.fill();
+        }
+      }
+      $(".console_container").css('display','block');
+      $("#submit_button").attr('value','Compare').css('display','inline-block').removeAttr("disabled");
+      $("#loading_gif").remove();
+
+      //compare faces
+      //left first
+
+      for (i=0;i<leftImgObj.result.length;i++){
+        $("#left_box_"+i).click(function(){
+          var thisId = parseInt($(this).attr("id").slice(9));
+          if (leftSelected != thisId){
+            $("#left_box_"+leftSelected).css('border-color','black').css('background-color','transparent').find('canvas').css('display','none');
+
+            var tmpStr = "<p>";
+            tmpStr += "Picture Size : "+leftImgObj.originalWidth+"x"+leftImgObj.originalHeight;
+            tmpStr += '<br />';
+            tmpStr += "Face Size : "+leftImgObj.result[thisId].w+"x"+leftImgObj.result[thisId].h;
+            tmpStr += "<br />";
+            tmpStr += "Pictrue Id: "+leftImgObj.serverId+"     Face Id: "+thisId;
+            tmpStr += "</p>";
+
+            $("#left_console_box").html(tmpStr).find("p").css('margin-left','10'); 
+            $(this).css('border-color','red');
+            $(this).css('background-color','rgba(255,255,255,0.5)');
+            $(this).find("canvas").css("display", "inline-block");
+            leftSelected = thisId;
+          } else {
+            leftSelected = -1;
+            $("#left_console_box").html(consoleMessageStr);
+            $(this).css('border-color','black');
+          }
+        });
+      }
+
+      for (i=0;i<rightImgObj.result.length;i++){
+        $("#right_box_"+i).click(function(){
+          var thisId = parseInt($(this).attr("id").slice(10));
+          if (rightSelected != thisId) {
+            $("#right_box_"+rightSelected).css('border-color','black').css('background-color','transparent').find('canvas').css('display','none');
+
+            var tmpStr = "<p>";
+            tmpStr += "Picture Size : "+rightImgObj.originalWidth+"x"+rightImgObj.originalHeight;
+            tmpStr += '<br />';
+            tmpStr += "Face Size : "+rightImgObj.result[thisId].w+"x"+rightImgObj.result[thisId].h;
+            tmpStr += "<br />";
+            tmpStr += "Pictrue Id: "+rightImgObj.serverId+"     Face Id: "+thisId;
+            tmpStr += "</p>";
+
+            $("#right_console_box").html(tmpStr).find("p").css('margin-left','10');
+            $(this).css('border-color','red');
+            $(this).css('background-color','rgba(255,255,255,0.5)');
+            $(this).find("canvas").css("display", "inline-block");
+            rightSelected = thisId;
+          } else {
+            rightSelected = -1;
+            $("#right_console_box").html(consoleMessageStr);
+            $(this).css('border-color','black');
+          }
+        });
+      }
+
+      //put some instructions into box div
+      if (leftImgObj.result.length > 1) { $("#left_console_box").html(consoleMessageStr); }
+      if (rightImgObj.result.length > 1) { $("#right_console_box").html(consoleMessageStr); }
+      //error for no faces
+      if (leftImgObj.result.length == 0) {
+        var tmpStr = "<p class='console_error'>ERROR : No faces were found on this picture.</p>";
+        $("#left_console_box").html(tmpStr);
+      }
+      if (rightImgObj.result.length == 0) {
+        var tmpStr = "<p class='console_error'>ERROR : No faces were found on this picture.</p>";
+        $("#right_console_box").html(tmpStr);
+      }
+      $(".result_box").css('display','block').height(20);
+      $("#result_box_p").text("Dectetion Time : "+detectionTime+"ms");
+      $("#result_box_h").css('display','none');
+      $("#result_box_h3").css('display','none');
+
+      //auto click for one face
+      if (leftImgObj.result.length == 1) { $("#left_box_0").click(); }
+      if (rightImgObj.result.length == 1) { $("#right_box_0").click(); }
+
+      //if there is only one face on each pic, compare them automaticly
+      if (leftSelected == 0 && rightSelected == 0 && (! isComparing)) {
+        //click button
+        compareFace(0,0);
+      }
+    }
+  });
+}
+
+function compareFace (faceId1, faceId2) {
+  $("#submit_button").css("display", "none");
+  $(".submit_box").find("p").prepend("<img src='image/loading.gif' id='loading_gif'>");
+  console.log(faceId1 + " , " + faceId2);
+  
+  $.post("http://10.193.251.172:8081/face_api", {
+    'method' : 'compare_image',
+    'img_id_1' : leftImgObj.serverId,
+    'img_id_2' : rightImgObj.serverId,
+    'rect_id_1' : faceId1,
+    'rect_id_2' : faceId2
+  }, function(data, textStatus, jqXHR){
+    $(".result_box").css('height','100px');
+    $("#result_box_p").css('display','block').html("<p id='result_box_p'>Comparing Time : " + data.time + "ms<br />Detection Time : " + detectionTime + "ms");
+    $("#result_box_h").css('display','block').text("Result(0~1) : " + data.result);
+    $("#result_box_h3").css('display','block');
+    
+    $("#submit_button").css('display','inline-block');
+    $("#loading_gif").remove();
+  }, 'json');
+}
---
+++ faceVeri/node/main.js
@@ -0,0 +1,276 @@
+//// FACE RECOGNIZATION API SERVER 
+// main.js
+// Version : 1.0
+
+var currentDirectory = "/home/liuyuxuan/dev/node/face_api_plus/"
+
+//Requirements here
+var http = require('http');
+var net = require('net');
+var fs = require('fs');
+var formidable = require('formidable');
+var urlParser = require('url').parse;
+var util = require('util');
+var sys = require("sys");
+var spawn = require('child_process').spawn;
+// var $ = require('jQuery'); // jquery 1.7.2
+var htmlparser = require('htmlparser');
+
+var queue = new Array();
+var currentQueueId = 0;
+
+function constructDetectXmlFile (imgPath1, imgPath2) {
+  var resultStr = '<?xml version = "1.0"?><input><path>';
+  resultStr += imgPath1;
+  resultStr += "</path><path>";
+  resultStr += imgPath2;
+  resultStr += "</path></input>"
+  return resultStr;
+}
+
+function constructRecXmlFile (imgId1, rectId1, imgId2, rectId2) {
+  // console.log(rectId1 + ", " + rectId2);
+
+  var resultStr = '<?xml version = "1.0"?><output><result><path>';
+  resultStr += queue[imgId1].fileName;
+  resultStr += "</path><rec><x>";
+  resultStr += queue[imgId1].result[rectId1].x;
+  resultStr += "</x><y>";
+  resultStr += queue[imgId1].result[rectId1].y;
+  resultStr += "</y><w>";
+  resultStr += queue[imgId1].result[rectId1].w;
+  resultStr += "</w><h>";
+  resultStr += queue[imgId1].result[rectId1].h;
+  resultStr += "</h>";
+  for (i=0; i<27; i++) {
+    resultStr += ("<point><x>" + queue[imgId1].result[rectId1].points[i].x + "</x>");
+    resultStr += ("<y>" + queue[imgId1].result[rectId1].points[i].y + "</y></point>");
+  }
+
+  resultStr += "</rec></result><result><path>"
+  resultStr += queue[imgId2].fileName;
+  resultStr += "</path><rec><x>";
+  resultStr += queue[imgId2].result[rectId2].x;
+  resultStr += "</x><y>";
+  resultStr += queue[imgId2].result[rectId2].y;
+  resultStr += "</y><w>";
+  resultStr += queue[imgId2].result[rectId2].w;
+  resultStr += "</w><h>";
+  resultStr += queue[imgId2].result[rectId2].h;
+  resultStr += "</h>";
+  for (i=0; i<27; i++) {
+    resultStr += ("<point><x>" + queue[imgId2].result[rectId2].points[i].x + "</x>");
+    resultStr += ("<y>" + queue[imgId2].result[rectId2].points[i].y + "</y></point>");
+  }  
+
+  resultStr += "</rec></result></output>";
+
+  return resultStr;
+}
+
+function parseXmlImgData (imgDomObj) {
+  var resultArr = new Array();
+  var facenum = parseInt(imgDomObj[1].children[0].data);
+  for (i=0;i<facenum;i++){
+    var tmpArr = imgDomObj[2+i].children;
+    var tx = parseInt(tmpArr[0].children[0].data);
+    var ty = parseInt(tmpArr[1].children[0].data);
+    var tw = parseInt(tmpArr[2].children[0].data);
+    var th = parseInt(tmpArr[3].children[0].data);
+    var tpoints = new Array();
+    for (j=0;j<27;j++){
+      var p_x = parseInt(tmpArr[4+j].children[0].children[0].data)
+      var p_y = parseInt(tmpArr[4+j].children[1].children[0].data)
+      tpoints.push({ x : p_x, y : p_y });
+    }
+
+    resultArr.push({
+      rectId : i,
+      x : tx, y : ty, w: tw, h : th,
+      points : tpoints
+    });
+  }
+
+  return resultArr;
+}
+
+http.createServer(function (req, res) {
+  if (req.method.toLowerCase() == "post") {
+    var urlPath = urlParser(req.url).pathname;
+    if (urlPath == "/face_api") {
+      var form = new formidable.IncomingForm();
+      form.uploadDir = "./images/";
+      form.encoding = 'utf-8';
+      form.keepExtensions = true;
+
+      form.parse(req, function(err, fields, files) {
+        res.writeHead(200, {'content-type': 'text/plain', 'Access-Control-Allow-Origin': '*'});
+
+        if (! fields.method) {
+          res.end("You have to specify an API method !");
+          return;
+        }
+
+        switch (fields.method) {
+          case 'upload_image' :
+          //upload two images here
+            if (! files.img_file_1){
+              res.end("File not uploaded, use 'img_file_1' field.");
+              return;
+            }
+            if (! files.img_file_2){
+              res.end("File not uploaded, use 'img_file_2' field.");
+              return;
+            }
+
+            if ((files.img_file_1.size <= 0) || (files.img_file_2.size <= 0)) {
+              res.end("Illegal File Size"); 
+              return;
+            }
+
+            var imgId1 = currentQueueId;    
+            var imgId2 = currentQueueId+1;
+            currentQueueId+=2;
+
+            var imgObj1 = {
+              id : imgId1,
+              fileName : currentDirectory + files.img_file_1.path,
+              fileSize : files.img_file_1.size,
+              addedTime : (Date()),
+              result : null
+            };
+
+            var imgObj2 = {
+              id : imgId2,
+              fileName : currentDirectory + files.img_file_2.path,
+              fileSize : files.img_file_2.size,
+              addedTime : (Date()),
+              result : null 
+            };
+
+            queue.push(imgObj1);
+            queue.push(imgObj2);
+
+            var xmlStr = constructDetectXmlFile(imgObj1.fileName, imgObj2.fileName);
+            var inputXmlFileName = currentDirectory + "xml/"+imgId1+".input.xml";
+            var outputXmlFileName = currentDirectory + "xml/"+imgId1+".output.xml";
+            fs.writeFileSync(inputXmlFileName,xmlStr);
+            console.log(xmlStr);
+
+            //start program
+            var startDate = new Date(); //check time here
+
+            var connection = new net.Socket();
+            var connectionBody = '';
+            connection.connect(8888,'10.193.251.173');
+            connection.write(inputXmlFileName + "#" + outputXmlFileName + "#" + "detect");
+            connection.on('data', function(d){connectionBody += d});
+
+            // var newExec = spawn('/home/liuyuxuan/dev/xml_interface/project/detxml' , 
+            //   [inputXmlFileName, 
+            //    outputXmlFileName, 
+            //    '/home/liuyuxuan/dev/faceDet/project/Models/f_cascade.xml', 
+            //    '/home/liuyuxuan/dev/faceDet/project/Models/models.file']
+            // );
+
+            connection.on('close', function(){
+              //program end
+              connection.destroy();
+              console.log(connectionBody);
+              var endDate = new Date();
+              console.log("Time : "+(endDate-startDate)+"ms");
+              var outputXmlStr = fs.readFileSync(outputXmlFileName);
+
+              //start xml parser
+              var xmlHandler = new htmlparser.DefaultHandler(function (error, dom){ }, { verbose: false, ignoreWhitespace: true });
+              var xmlParser = new htmlparser.Parser(xmlHandler);
+              xmlParser.parseComplete(outputXmlStr);
+              // console.log(xmlHandler.dom[1].children[0].children[1]);
+              var imgResult1 = parseXmlImgData(xmlHandler.dom[1].children[0].children);
+              var imgResult2 = parseXmlImgData(xmlHandler.dom[1].children[1].children);
+              // console.log(xmlHandler.dom[1].children[1].children)
+              // console.log(imgResult1);
+              queue[imgId1].result = imgResult1;
+              queue[imgId2].result = imgResult2;
+              
+              responseObj = {
+                img1 : { id : imgId1 , result : imgResult1 } , 
+                img2 : { id : imgId2 , result : imgResult2 } ,
+                time : (endDate-startDate)
+              };
+              res.end(JSON.stringify(responseObj));
+            });
+
+            return;
+
+          case 'compare_image' :
+            var imgId1 = parseInt(fields.img_id_1);
+            var imgId2 = parseInt(fields.img_id_2);
+            if ((imgId1 >= currentQueueId) || (imgId2 >= currentQueueId)) {
+              res.end("Illegal Image ID"); return;
+            }
+
+            var rectId1 = parseInt(fields.rect_id_1);
+            var rectId2 = parseInt(fields.rect_id_2);
+            if ((rectId1 >= queue[imgId1].result.length) || (rectId2 >= queue[imgId2].result.length)) {
+              res.end("Illegal RECT ID"); return;
+            }
+
+            var XmlStr = constructRecXmlFile(imgId1, rectId1, imgId2, rectId2);
+            var recInputXmlFileName = currentDirectory + "xml/rec.input.xml";
+            var recOutputFileName = currentDirectory + "xml/rec.output.txt";
+            fs.writeFileSync(recInputXmlFileName,XmlStr);
+            console.log(XmlStr);
+
+            var startDate = new Date();
+            var connection = new net.Socket();
+            var connectionBody = '';
+            connection.connect(8888,'10.193.251.173');
+            connection.write(recInputXmlFileName + "#" + recOutputFileName + "#" + "recognition");
+            connection.on('data', function(d){connectionBody += d});
+
+            // var recExec = spawn('/home/liuyuxuan/dev/xml_interface/project/recxml',[
+            //   recInputXmlFileName,
+            //   '/home/liuyuxuan/dev/xml_interface/project/Models/f_cascade.xml',
+            //   '/home/liuyuxuan/dev/xml_interface/project/Models/models.file',
+            //   '/home/liuyuxuan/dev/xml_interface/project/rModels/model.bin',
+            //   '/home/liuyuxuan/dev/xml_interface/project/rModels/paras.bin',
+            //   '/home/liuyuxuan/dev/xml_interface/project/rModels/hmodel.bin',
+            //   '/home/liuyuxuan/dev/xml_interface/project/rModels/hparas.bin',
+            //   recOutputFileName
+            // ]);
+
+            connection.on('close',function(){
+              connection.destroy();
+              console.log(connectionBody);
+              var endDate = new Date();
+              console.log("Time : "+(endDate-startDate)+"ms");
+              var outputStr = fs.readFileSync(recOutputFileName);
+              var resultNumber = parseFloat(outputStr);
+              console.log("Result : " + resultNumber);
+
+              responseObj = {
+                result : resultNumber,
+                time : (endDate-startDate)
+              };
+
+              res.end(JSON.stringify(responseObj));
+            });
+            return;
+
+          default :
+            res.end("Unknown method.");
+            return;
+
+          //END CASE
+        }
+      });
+    }
+  } else {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('{ "error" : "PLEASE USE HTTP POST METHOD"}');
+  }
+
+}).listen(8082, '10.193.251.172');
+
+console.log('Socket API server running at http://10.193.251.172:8082/face_api');
